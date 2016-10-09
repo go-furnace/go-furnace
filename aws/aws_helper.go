dbcdfd1bbc388e5749a45417979a40bde02057b0
Gergely Brautigam <skarlso777@gmail.com>
2016-10-09 22:46:54 +0200

    Skeleton building.

--- aws/aws_helper.go
+++ aws/aws_helper.go
@@ -1,14 +1,21 @@
 package aws
 
 import (
-  "fmt"
+  "log"
 
   "github.com/aws/aws-sdk-go/aws"
   "github.com/aws/aws-sdk-go/aws/session"
+  "github.com/aws/aws-sdk-go/service/ec2"
 )
 
 // TestAWS testing AWS configuration.
 func TestAWS() {
+  log.Println("Creating ec2 session.")
   sess := session.New(&aws.Config{Region: aws.String("eu-central-1")})
-  fmt.Println(sess)
+  svc := ec2.New(sess, nil)
+  resp, err := svc.DescribeInstances(nil)
+	if err != nil {
+		panic(err)
+	}
+  log.Println(resp)
 }
---
+++ cfg/config_loader.go
@@ -0,0 +1,12 @@
+package cfg
+
+// Configuration is a Configuration object.
+type Configuration struct {
+  LogLevel string
+  UploadPath string
+}
+
+// LoadConfiguration Loads the configuration file.
+func LoadConfiguration() {
+
+}
