1565591224f5d1539ed9b02fb04a4a3b70e81940
Gergely Brautigam <skarlso777@gmail.com>
2016-10-10 15:08:21 +0200

    Renamed main and playing with commands.

--- commands/create_ec2.go
+++ commands/create_ec2.go
@@ -1,29 +1,34 @@
 package commands
 
 import (
-  "log"
+	"log"
 )
 
-// Yout Command
+// CreateEC2 command.
 type CreateEC2 struct {
 }
 
+// Execute defines what this command does.
 func (c *CreateEC2) Execute() {
-  log.Println("Running CreateEC2 command")
+	log.Println("Running CreateEC2 command")
 }
 
+// ArgumentDescription describes the arguments for this command.
 func (c *CreateEC2) ArgumentDescription() string {
-  return "[create-ec2]"
+	return "[create-ec2]"
 }
 
+// Description is the description of this command.
 func (c *CreateEC2) Description() string {
-  return "Create an EC2 instance to run the server on."
+	return "Create an EC2 instance to run the server on."
 }
 
+// Help displays help information.
 func (c *CreateEC2) Help() string {
-  return "go_aws_mine create-ec2"
+	return "go_aws_mine create-ec2"
 }
 
+// Examples will be displayed by 'help create-ec2'.
 func (c *CreateEC2) Examples() []string {
-  return []string{"", "test"}
+	return []string{"", "test"}
 }
---
+++ go-mine.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	// "github.com/Skarlso/go_aws_mine/aws"
+	"github.com/Skarlso/go_aws_mine/commands"
+	cmd "github.com/Yitsushi/go-commander"
+)
+
+func main() {
+	// aws.TestAWS()
+	registry := cmd.NewCommandRegistry()
+	registry.Register("create-ec2", &commands.CreateEC2{})
+	registry.Execute()
+}
