0251f56b9e047426f9c99bb8a9311de8bbb512d3
Gergely Brautigam <skarlso777@gmail.com>
2016-10-23 12:27:56 +0200

    Waiting for an instance to be running.

--- goaws/aws.go
+++ goaws/aws.go
@@ -15,7 +15,7 @@ func CreateEC2(ec2Config *config.EC2Config) {
 	log.Println("Creating ec2 session.")
 	sess := session.New(&aws.Config{Region: aws.String("eu-central-1")})
 	ec2Client := ec2.New(sess, nil)
-	ec2Client.RunInstances(&ec2.RunInstancesInput{
+	runResult, err := ec2Client.RunInstances(&ec2.RunInstancesInput{
 		ImageId:      &ec2Config.ImageID,
 		DryRun:       &ec2Config.DryRun,
 		MaxCount:     &ec2Config.MaxCount,
@@ -24,9 +24,11 @@ func CreateEC2(ec2Config *config.EC2Config) {
 		KeyName:      &ec2Config.KeyName,
 		Monitoring:   &ec2.RunInstancesMonitoringEnabled{Enabled: &ec2Config.Monitoring.Enable},
 	})
-	resp, err := ec2Client.DescribeInstances(nil)
+	log.Println("Instance created with id: ", *runResult.Instances[0].InstanceId)
+	errorhandler.CheckError(err)
+	ec2Id := aws.StringSlice([]string{*runResult.Instances[0].InstanceId})
+	err = ec2Client.WaitUntilInstanceRunning(&ec2.DescribeInstancesInput{InstanceIds: ec2Id})
 	if err != nil {
 		errorhandler.CheckError(err)
 	}
-	log.Println(resp)
 }
