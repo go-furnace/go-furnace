df0800066b5bf572bb27b7fe9bcd852c33abd7fb
Gergely Brautigam <skarlso777@gmail.com>
2017-02-23 09:37:49 +0100

    Actually testing something. :P

--- commands/create_test.go
+++ commands/create_test.go
@@ -4,13 +4,21 @@ import (
 	"testing"
 
 	"github.com/Skarlso/go-furnace/config"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudformation"
 	"github.com/aws/aws-sdk-go/service/cloudformation/cloudformationiface"
 )
 
+var notEmptyStack = &cloudformation.DescribeStacksOutput{
+	Stacks: []*cloudformation.Stack{
+		&cloudformation.Stack{StackName: aws.String("TestStack")},
+	},
+}
+
 type fakeCFClient struct {
 	cloudformationiface.CloudFormationAPI
-	err error
+	stackname string
+	err       error
 }
 
 func (fc *fakeCFClient) ValidateTemplate(input *cloudformation.ValidateTemplateInput) (*cloudformation.ValidateTemplateOutput, error) {
@@ -18,6 +26,9 @@ func (fc *fakeCFClient) ValidateTemplate(input *cloudformation.ValidateTemplateI
 }
 
 func (fc *fakeCFClient) CreateStack(input *cloudformation.CreateStackInput) (*cloudformation.CreateStackOutput, error) {
+	if fc.stackname == "NotEmptyStack" {
+		return &cloudformation.CreateStackOutput{StackId: aws.String("DummyID")}, fc.err
+	}
 	return &cloudformation.CreateStackOutput{}, fc.err
 }
 
@@ -26,14 +37,23 @@ func (fc *fakeCFClient) WaitUntilStackCreateComplete(input *cloudformation.Descr
 }
 
 func (fc *fakeCFClient) DescribeStacks(input *cloudformation.DescribeStacksInput) (*cloudformation.DescribeStacksOutput, error) {
+	if fc.stackname == "NotEmptyStack" {
+		return notEmptyStack, fc.err
+	}
 	return &cloudformation.DescribeStacksOutput{}, fc.err
 }
 
-func TestCreateCommand(t *testing.T) {
+func TestCreateProcedure(t *testing.T) {
 	config.WAITFREQUENCY = 0
 	client := new(CFClient)
-	client.Client = &fakeCFClient{err: nil}
+	stackname := "NotEmptyStack"
+	client.Client = &fakeCFClient{err: nil, stackname: stackname}
 	config := []byte("{}")
-	stackname := "TestStack"
-	create(stackname, config, client)
+	stacks := create(stackname, config, client)
+	if len(stacks) == 0 {
+		t.Fatal("Stack was not returned by create.")
+	}
+	if *stacks[0].StackName != "TestStack" {
+		t.Fatal("Not the correct stack returned. Returned was:", stacks)
+	}
 }
