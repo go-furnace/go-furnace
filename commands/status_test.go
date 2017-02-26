199b6c1f4179087419e36fd187863998582aef74
Gergely Brautigam <skarlso777@gmail.com>
2017-02-26 11:24:48 +0100

    Some more tests.

--- commands/status_test.go
+++ commands/status_test.go
@@ -20,12 +20,22 @@ func (fc *fakeStatusCFClient) DescribeStacks(input *cloudformation.DescribeStack
 	return &cloudformation.DescribeStacksOutput{}, fc.err
 }
 
-func TestStatusCommand(t *testing.T) {
+func TestStatusCommandWithStackReturned(t *testing.T) {
 	stackname := "NotEmptyStack"
 	client := new(CFClient)
 	client.Client = &fakeCreateCFClient{err: nil, stackname: stackname}
 	stacks := stackStatus(stackname, client)
 	if len(stacks.Stacks) == 0 {
+		t.Fatal("Zero stacks returned: ", stacks)
+	}
+}
+
+func TestStatusWithNoStacks(t *testing.T) {
+	stackname := "EmptyStacks"
+	client := new(CFClient)
+	client.Client = &fakeCreateCFClient{err: nil, stackname: stackname}
+	stacks := stackStatus(stackname, client)
+	if len(stacks.Stacks) != 0 {
 		t.Fatal("Zero stacks returned: ", stacks)
 	}
 }
