f43324678a9510964789b8ce71ca0d8dd5deb9eb
Gergely Brautigam <skarlso777@gmail.com>
2017-03-06 20:28:28 +0100

    Delete app test

--- commands/delete_app_test.go
+++ commands/delete_app_test.go
@@ -1,1 +1,25 @@
 package commands
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/codedeploy"
+	"github.com/aws/aws-sdk-go/service/codedeploy/codedeployiface"
+)
+
+type fakeDeleteAppCDClient struct {
+	codedeployiface.CodeDeployAPI
+	err    error
+	awsErr awserr.Error
+}
+
+func (fd *fakeDeleteAppCDClient) DeleteApplication(*codedeploy.DeleteApplicationInput) (*codedeploy.DeleteApplicationOutput, error) {
+	return &codedeploy.DeleteApplicationOutput{}, fd.err
+}
+
+func TestDeletingApplication(t *testing.T) {
+	client := new(CDClient)
+	client.Client = &fakeDeleteAppCDClient{err: nil, awsErr: nil}
+	deleteApplication("fakeApp", client)
+}
