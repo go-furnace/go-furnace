11e3dd8d5db5dcd29b89b3f9e2ea44a64965b934
Gergely Brautigam <skarlso777@gmail.com>
2017-03-18 14:41:14 +0100

    Added resource display to status

--- commands/status.go
+++ commands/status.go
@@ -17,6 +17,13 @@ import (
 type Status struct {
 }
 
+type ResourceStatus struct {
+	Status     string
+	PhysicalId string
+	LogicalId  string
+	Type       string
+}
+
 // Execute defines what this command does.
 func (c *Status) Execute(opts *commander.CommandHelper) {
 	stackname := config.STACKNAME
@@ -26,6 +33,8 @@ func (c *Status) Execute(opts *commander.CommandHelper) {
 	stack := stackStatus(stackname, &client)
 	info := color.New(color.FgWhite, color.Bold).SprintFunc()
 	log.Println("Stack state is: ", info(stack.Stacks[0].GoString()))
+	stackResources := stackResources(stackname, &client)
+	printStackResources(stackResources)
 }
 
 func stackStatus(stackname string, cfClient *CFClient) *cloudformation.DescribeStacksOutput {
@@ -33,6 +42,34 @@ func stackStatus(stackname string, cfClient *CFClient) *cloudformation.DescribeS
 	utils.CheckError(err)
 	fmt.Println()
 	return descResp
+}
+
+func stackResources(stackname string, cfClient *CFClient) []ResourceStatus {
+	resources := make([]ResourceStatus, 0)
+	descResp, err := cfClient.Client.DescribeStackResources(&cloudformation.DescribeStackResourcesInput{StackName: aws.String(stackname)})
+	utils.CheckError(err)
+	for _, r := range descResp.StackResources {
+		res := ResourceStatus{Status: *r.ResourceStatus, PhysicalId: *r.PhysicalResourceId, LogicalId: *r.LogicalResourceId, Type: *r.ResourceType}
+		resources = append(resources, res)
+	}
+	fmt.Println()
+	return resources
+}
+
+func printStackResources(resources []ResourceStatus) {
+	info := color.New(color.FgWhite, color.Bold).SprintFunc()
+	fmt.Println(info("___________________"))
+	for _, r := range resources {
+		fmt.Print(info(r))
+	}
+	fmt.Println()
+}
+
+func (r ResourceStatus) String() string {
+	ret := ""
+	ret += fmt.Sprintf("|Name:          %s|\n|Id:            %s|\n|Status:        %s|\n|Type:          %s|\n", r.LogicalId, r.PhysicalId, r.Status, r.Type)
+	ret += "-------------------\n"
+	return ret
 }
 
 // NewStatus Creates a new Status command.
