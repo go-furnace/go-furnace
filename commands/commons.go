dc922c2a4cced4ae3d5988fd0ae22c113e094468
Gergely Brautigam <skarlso777@gmail.com>
2017-07-08 12:24:55 +0200

    Little refactoring.

---
+++ commands/commons.go
@@ -0,0 +1,66 @@
+package commands
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/Skarlso/go-furnace/config"
+	"github.com/Skarlso/go-furnace/utils"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/fatih/color"
+)
+
+func gatherParameters(source *os.File, params *cloudformation.ValidateTemplateOutput) []*cloudformation.Parameter {
+	var stackParameters []*cloudformation.Parameter
+	defaultValue := color.New(color.FgHiBlack, color.Italic).SprintFunc()
+	log.Println("Gathering parameters.")
+	for _, v := range params.Parameters {
+		var param cloudformation.Parameter
+		fmt.Printf("%s - '%s'(%s):", *v.Description, keyName(*v.ParameterKey), defaultValue(*v.DefaultValue))
+		text := readInputFrom(source)
+		param.SetParameterKey(*v.ParameterKey)
+		text = strings.Trim(text, "\n")
+		if len(text) > 0 {
+			param.SetParameterValue(*aws.String(text))
+		} else {
+			param.SetParameterValue(*v.DefaultValue)
+		}
+		stackParameters = append(stackParameters, &param)
+	}
+	return stackParameters
+}
+
+func readInputFrom(source *os.File) string {
+	reader := bufio.NewReader(source)
+	text, _ := reader.ReadString('\n')
+	return text
+}
+
+func (cf *CFClient) waitForStackStatus(stackname string, status string) {
+	describeStackInput := &cloudformation.DescribeStacksInput{
+		StackName: aws.String(stackname),
+	}
+	utils.WaitForFunctionWithStatusOutput(status, config.WAITFREQUENCY, func() {
+		cf.Client.WaitUntilStackCreateComplete(describeStackInput)
+	})
+}
+
+func (cf *CFClient) describeStacks(descStackInput *cloudformation.DescribeStacksInput) *cloudformation.DescribeStacksOutput {
+	descResp, err := cf.Client.DescribeStacks(descStackInput)
+	utils.CheckError(err)
+	return descResp
+}
+
+func (cf *CFClient) validateTemplate(template []byte) *cloudformation.ValidateTemplateOutput {
+	log.Println("Validating template.")
+	validateParams := &cloudformation.ValidateTemplateInput{
+		TemplateBody: aws.String(string(template)),
+	}
+	resp, err := cf.Client.ValidateTemplate(validateParams)
+	utils.CheckError(err)
+	return resp
+}
