4c6cabc03bdafcc5d6d12c7e784ab081c67b0f02
Gergely Brautigam <skarlso777@gmail.com>
2018-09-15 08:35:45 +0200

    Working on postcreate.

--- furnace-aws/plugins/plugin.go
+++ furnace-aws/plugins/plugin.go
@@ -75,20 +75,65 @@ func (m *GRPCPreCreateServer) Execute(ctx context.Context, req *proto.Stack) (*p
 // implemented and used via plugins. This interface gives access to the
 // stack name.
 type PostCreate interface {
-	Execute(key string)
+	Execute(stackname string)
 }
 
 // PreCreate is the interface for anything before the build happens. The
 // PreCreate plugin has the change to abort the build if returns false.
 type PreCreate interface {
-	Execute(key string) bool
+	Execute(stackname string) bool
+}
+
+// PostCreateGRPCPlugin is the implementation of plugin.GRPCPlugin so we can serve/consume this.
+type PostCreateGRPCPlugin struct {
+	// GRPCPlugin must still implement the Plugin interface
+	plugin.Plugin
+	// Concrete implementation, written in Go. This is only used for plugins
+	// that are written in Go.
+	Impl PostCreate
+}
+
+// GRPCPostCreateClient is an implementation of PreCreate that talks over RPC.
+type GRPCPostCreateClient struct{ client proto.PostCreateClient }
+
+// Execute is the GRPC implementation of the Execute function for the
+// PostCreate plugin definition. This will talk over GRPC.
+func (m *GRPCPostCreateClient) Execute(stackname string) {
+	m.client.Execute(context.Background(), &proto.Stack{
+		Name: stackname,
+	})
+}
+
+// GRPCPostCreateServer is the gRPC server that GRPCPostCreateClient talks to.
+type GRPCPostCreateServer struct {
+	// This is the real implementation
+	Impl PostCreate
+}
+
+// GRPCServer is the grpc server implementation which calls the
+// protoc generated code to register it.
+func (p *PostCreateGRPCPlugin) GRPCServer(broker *plugin.GRPCBroker, s *grpc.Server) error {
+	proto.RegisterPostCreateServer(s, &GRPCPostCreateServer{Impl: p.Impl})
+	return nil
+}
+
+// GRPCClient is the grpc client that will talk to the GRPC Server
+// and calls into the generated protoc code.
+func (p *PostCreateGRPCPlugin) GRPCClient(ctx context.Context, broker *plugin.GRPCBroker, c *grpc.ClientConn) (interface{}, error) {
+	return &GRPCPostCreateClient{client: proto.NewPostCreateClient(c)}, nil
+}
+
+// Execute is the execute functin of the GRPCServer which will rely the information to the
+// underlying implementation of this interface.
+func (m *GRPCPostCreateServer) Execute(ctx context.Context, req *proto.Stack) (*proto.Empty, error) {
+	m.Impl.Execute(req.Name)
+	return &proto.Empty{}, nil
 }
 
 // RunPreCreatePlugins will execute all the PreCreate plugins. This function
 // uses plugin discovery via the glob:
 // PreCreate plugins: `*-furnace-precreate`
 func RunPreCreatePlugins(stackname string) {
-	// TODO: Fill the plugin map with the names of the plugins..?
 	ps, _ := discoverPlugins("*-furnace-precreate")
 	pluginMap := make(map[string]plugin.Plugin, 0)
 	for _, v := range ps {
@@ -129,11 +174,45 @@ func RunPreCreatePlugins(stackname string) {
 	}
 }
 
+// TODO: Find a way to unify this.
 // RunPostCreatePlugins will execute all the PreCreate plugins. This function
 // uses plugin discovery via the glob:
 // PostCreate plugins: `*-furnace-postcreate`
 func RunPostCreatePlugins(stackname string) {
+	ps, _ := discoverPlugins("*-furnace-postcreate")
+	pluginMap := make(map[string]plugin.Plugin, 0)
+	for _, v := range ps {
+		pluginName := filepath.Base(v)
+		pluginMap[pluginName] = &PostCreateGRPCPlugin{}
+	}
 
+	for _, v := range ps {
+		client := plugin.NewClient(&plugin.ClientConfig{
+			HandshakeConfig: Handshake,
+			Plugins:         pluginMap,
+			Cmd:             exec.Command(v),
+			AllowedProtocols: []plugin.Protocol{
+				plugin.ProtocolGRPC},
+		})
+
+		defer client.Kill()
+		grpcClient, err := client.Client()
+		if err != nil {
+			fmt.Println("Error:", err.Error())
+			os.Exit(1)
+		}
+
+		pluginName := filepath.Base(v)
+		// Request the plugin
+		raw, err := grpcClient.Dispense(pluginName)
+		if err != nil {
+			fmt.Println("Error:", err.Error())
+			os.Exit(1)
+		}
+
+		p := raw.(PostCreate)
+		p.Execute(stackname)
+	}
 }
 
 func discoverPlugins(postfix string) (p []string, err error) {
