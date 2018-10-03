1f410849e6432d5e8d23f4bade9c33537ae17b10
Gergely Brautigam <skarlso777@gmail.com>
2018-10-03 16:13:35 +0200

    Fixed file.

--- test-coverage.sh
+++ test-coverage.sh
@@ -4,7 +4,7 @@ echo "mode: set" > acc.out
 fail=0
 
 # Standard go tooling behavior is to ignore dirs with leading underscors
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d);
+for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path './vendor*' -not -path '*/_*' -type d);
 do
   if ls $dir/*.go &> /dev/null; then
     go test -coverprofile=profile.out $dir || fail=1
