c18fa0a25b05b5f13396bd6b295a2f250da5bd27
Gergely Brautigam <skarlso777@gmail.com>
2019-03-10 11:47:26 +0100

    Extracting and moving around code a bit. (#49)

---
+++ furnace-aws/commands/validator.go
@@ -0,0 +1,20 @@
+package commands
+
+import (
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
+	"github.com/go-furnace/go-furnace/handle"
+)
+
+func (cf *CFClient) validateTemplate(template []byte) *cloudformation.ValidateTemplateOutput {
+	log.Println("Validating template.")
+	validateParams := &cloudformation.ValidateTemplateInput{
+		TemplateBody: aws.String(string(template)),
+	}
+	req := cf.Client.ValidateTemplateRequest(validateParams)
+	resp, err := req.Send()
+	handle.Error(err)
+	return resp
+}
